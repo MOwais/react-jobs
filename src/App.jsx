ed33b657111ce01f4281840fd5fadb35ef3a06a0
MOwais <owace.ahmed@gmail.com>
2025-01-11 19:15:39 -0800

    job page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import MainLayout from "./layouts/MainLayout";
 
@@ -15,6 +16,7 @@ const App = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
