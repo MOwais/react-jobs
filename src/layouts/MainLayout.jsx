cf004c4e2e04ba9693cac92aeab7568cb68a0274
MOwais <owace.ahmed@gmail.com>
2025-01-13 18:18:16 -0800

    add, edit, delete jobs

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/NavBar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
+      <ToastContainer />
       <Outlet />
     </>
   );
